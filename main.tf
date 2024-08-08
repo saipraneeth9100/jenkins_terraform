2aacc2dbccb829f6ab4841cb42fc21bd9215f8f3
Krishnakumarmorle <141539650+Krishnakumarmorle@users.noreply.github.com>
2024-08-08 12:15:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,7 @@
 provider "aws" {
     region = "us-east-1"  
 }
-
 resource "aws_instance" "tfjenkinstest" {
   ami           = "ami-04a81a99f5ec58529"
   instance_type = "t2.micro"
-  tags = {
-      Name = "tftest"
-  }
 }
